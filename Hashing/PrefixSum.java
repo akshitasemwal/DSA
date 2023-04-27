7236c4c6ae4a7dd968a9e9aaf8ecca407d6ac38a
akshita <akshitasemwal@gmail.com>
2023-04-27 20:10:39 +0530

    Updated solution 3: added explanation

--- Hashing/PrefixSum.java
+++ Hashing/PrefixSum.java
@@ -11,18 +11,31 @@ public class PrefixSum {
         int[] arr = {1, 4, 1, 6 , 2, 8};
         int num1 = 2;
         int num2 = 5;
-        prefixSum(arr, num1, num2);
+        prefixSum1(arr, num1, num2);
+        prefixSum2(arr, num1, num2);
     }
 
-    static void prefixSum(int[] arr, int num1, int num2){
+    static void prefixSum1(int[] arr, int num1, int num2){
+        int n = arr.length;
+        int[] prefixSum = new int[n];
+        prefixSum[0] = arr[0];
+        for(int i = 1; i < n; i++){
+            prefixSum[i] = prefixSum[i-1] + arr[i];
+        }
+        int number = prefixSum[num2] - prefixSum[num1-1];
+        System.out.println(number);
+    }
+
+    static void prefixSum2(int[] arr, int num1, int num2){
          for(int i = num1 + 1; i <= num2; i++){
              arr[i] += arr[i-1];
          }
-        System.out.print(arr[num2]);
+        System.out.println(arr[num2]);
     }
 }
 
 /*
-This solution uses the prefix sum technique to store successive sum of the elements in the array.
+[1] this solution uses conventional method of calculating the prefix sum for the entire array and then printing the required number.
+[2] this solution uses the prefix sum technique to store successive sum of the elements in the array.
 It stores the sum of all the elements upto that particular element, i.e, from index 0 to i.
  */
