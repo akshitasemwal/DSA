28fa5dbaf385a8c5bc0669a38e004770b275fa06
akshita <akshitasemwal@gmail.com>
2023-05-09 16:17:31 +0530

    Updated solution 10: updated explanation

--- Hashing/SubarraysHavingXOR.java
+++ Hashing/SubarraysHavingXOR.java
@@ -1,5 +1,9 @@
 /*
-Given an array of integers arr[] and a number m, count the number of subarrays having XOR of their elements as m.
+Given an array of integers A and an integer B.
+Find the total number of subarrays having bitwise XOR of all elements equals to B.
+https://www.interviewbit.com/problems/subarray-with-given-xor/
+TC: O(n)
+SC: O(n)
 */
 
 package Hashing;
@@ -7,44 +11,44 @@ import java.util.*;
 
 public class SubarraysHavingXOR {
     public static void main(String[] args) {
-        List<Integer> A = Arrays.asList(4, 2, 2, 6, 4);
-        int B = 6;
-        int n = 4;
-        subarraysXOR(A, B, n);
+        int[] arr = {4, 2, 2, 6, 4};
+        int k = 6;
+        System.out.println(subarraysXOR(arr, k));
     }
-    public static void subarraysXOR(List<Integer> A, int B, int n){
-        Map<Integer,Integer> hmap=new HashMap<>();
-        int prefixXor = 0;
+    public static int subarraysXOR(int[] arr, int k) {
+        int n = arr.length;
+        int xor = 0;
         int count = 0;
-        hmap.put(0,1);
-        for(int i=0; i<A.size(); i++)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for(int i = 0; i<n; i++)
         {
-            prefixXor = prefixXor^A.get(i);
-            if(hmap.containsKey( prefixXor^B ))
+            xor ^= arr[i];
+            if(map.containsKey(xor^k))
             {
-                count = count+hmap.get(prefixXor^B);
-                if(hmap.containsKey(prefixXor))
-                {
-                    int value = hmap.get(prefixXor);
-                    hmap.put(prefixXor,value+1);
-                }
-                else{
-                    hmap.put(prefixXor,1);
-                }
+                count += map.get(xor^k);
             }
-            else{
-                if(hmap.containsKey(prefixXor))
-                {
-                    int value = hmap.get(prefixXor);
-                    hmap.put(prefixXor,value+1);
-                }
-                else{
-                    hmap.put(prefixXor,1);
-                }
+            if(map.containsKey(xor))
+            {
+                map.put(xor, map.get(xor)+1);
+            }
+            else
+            {
+                map.put(xor, 1);
             }
         }
-        System.out.println(count);
+        return count;
     }
-    }
-
 }
+
+/*
+This solution works on the same principles as tha last two solutions.
+It finds the unknown, with the help of xor up till the current element, and given k.
+It uses property of xor:
+a^b = c
+b^c = a
+a^c = b
+
+i.e., xor ^ k = the unknown x.
+If this unknown x exists in the map, the number of subarrays with given k will be equal to the frequency of the xor^k.
+*/
