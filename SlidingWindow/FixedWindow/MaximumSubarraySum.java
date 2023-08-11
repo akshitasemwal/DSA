280ff8d31cf4287d980769a8f064ecb208f30700
akshita <akshitasemwal@gmail.com>
2023-08-12 02:21:04 +0530

    Added fixed sliding window solution

--- SlidingWindow/FixedWindow/MaximumSubarraySum.java
+++ SlidingWindow/FixedWindow/MaximumSubarraySum.java
@@ -20,10 +20,10 @@ public class MaximumSubarraySum {
         long max = 0;
         int i = 0;
         int j = 0;
-        while ( j < n )
+        while ( j < n )                            //[1]
         {
-            sum += arr.get(j);
-            if( j - i + 1 == k)
+            sum += arr.get(j);                     //[2]
+            if( j - i + 1 == k)                    //[3]
             {
                 max = Math.max( sum, max);
                 sum -= arr.get(i);
@@ -37,7 +37,16 @@ public class MaximumSubarraySum {
 
 /*
 This solution uses two pointers to mark the start and end of a sliding window.
-[1] we first calculate the sum of the initial window
-[2] next, we keep on moving the window ahead by one place- adding arr[i] and subtracting arr[j] from the sum.
-We then compare it with max value, for a fixed window size.
+This solution is the staple sliding window format.
+
+[1] we run a loop while the end of the window, i.e., the j pointer, doesn't reach the end of the array.
+
+[2] this statement adds the current element to the sum variable
+
+[3] if the size of the window equals k, we will now have to do 2 operations:
+-> calculating the result
+If the sum of the current window is maximum, it gets stored in the max variable.
+
+-> sliding the window forward by 1 character, and hence removing the effect of the ith index.
+Subtract the element at the ith index from sum to remove it from the total sum of the next window.
 */
